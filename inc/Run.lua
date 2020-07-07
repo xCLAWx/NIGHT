5d364ad6ba82037ed8ebfec4f244b3ca0a7247f4
xCLAWx <65922778+xCLAWx@users.noreply.github.com>
2020-07-07 17:44:18 +0000

    Update Run.lua

--- inc/Run.lua
+++ inc/Run.lua
@@ -250,9 +250,6 @@ end
 if msg.sender_user_id_ == 656329268 then 
 msg.TheRank =  'مطور السورس'
 msg.Rank = 1
-elseif msg.sender_user_id_ == 1153357069 then 
-msg.TheRank =   'مطور السورس '
-msg.Rank = 1
 elseif msg.sender_user_id_ == SUDO_ID then 
 msg.TheRankCmd = 'المطور ' 
 msg.TheRank = 'مطور اساسي ' 
